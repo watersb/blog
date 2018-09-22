1d0eef373dae97e5ba38db2e77b898d374ab3e24
Boyd Waters <waters.boyd+git@gmail.com>
2018-09-22 22:57:17 +0000

    NGINX - add a _nginx dir with server configs for this site

---
+++ meta/_nginx/restrict-file-permissions.sh
@@ -0,0 +1,9 @@
+find . -path ./.git -prune -o -type f -print0 | xargs -0 chmod 400
+find . -path ./.git -prune -o -type d -print0 | xargs -0 chmod 500
+
+chown -R nginx *.html *.ico *.png *.txt .htaccess
+chown -R nginx 2018 css img js meta
+
+chown -R root meta/_nginx
+chown -R root css/_scrapbook
+
