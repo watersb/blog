6c586e4e900e6f67785befd341555be61a9711c3
Boyd Waters <waters.boyd+git@gmail.com>
2018-09-25 10:28:43 -0600

    NGINX - set ownership of all files to root, then chown nginx a subset

--- meta/_nginx/restrict-file-permissions.sh
+++ meta/_nginx/restrict-file-permissions.sh
@@ -1,9 +1,8 @@
 find . -path ./.git -prune -o -type f -print0 | xargs -0 chmod 400
 find . -path ./.git -prune -o -type d -print0 | xargs -0 chmod 500
 
-chown -R nginx *.html *.ico *.png *.txt .htaccess
-chown -R nginx 2018 css img js meta
-
 chown -R root meta/_nginx
 chown -R root css/_scrapbook
 
+chown -R nginx *.html *.ico *.png *.txt .htaccess
+chown -R nginx 2018 css img js meta
