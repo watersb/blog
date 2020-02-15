cedbfa63b3fdceaba071ce3bd0817f720a275be8
Boyd Waters <waters.boyd@gmail.com>
2020-02-15 16:38:48 -0700

    JS cleanup dbg-size revisions

---
+++ css/_scrapbook/_debugging/dbg-size-simple.js
@@ -0,0 +1,15 @@
+/* 2020-02-15 16:35:27 */
+
+function getsize(event) {
+var p = document.getElementById('dbg_size');
+var s = window.getComputedStyle(p);
+
+p.innerHTML =
+  "  font-size:   " + s.fontSize   + "\n" +
+  "  font-family: " + s.fontFamily + "\n" +
+  "  screen:      " + window.innerWidth +" x " + window.innerHeight
+;
+}
+
+window.addEventListener("resize", getsize);
+window.addEventListener("load", getsize);
