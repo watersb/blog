1e6dda65086cb36246b86cb93f6b46a9ac899ecd
Boyd Waters <waters.boyd@gmail.com>
2020-03-05 04:09:41 -0700

    DBG JS - append a debug <pre> node with Javascript

--- css/_lib/_dbg/dbg.js
+++ css/_lib/_dbg/dbg.js
@@ -1,20 +1,30 @@
-/* 2020-02-15 16:35:27 */
+/* 2020-03-05 03:28:57 -0700 */
 
-function getsize(event) {
-const p = document.getElementById('dbg_size');
-const s = window.getComputedStyle(p);
-const w = window.innerWidth /parseFloat(s.fontSize);
-const h = window.innerHeight/parseFloat(s.fontSize);
+window.addEventListener("load",
+  () => {
+    var p = document.getElementById('_dbg_size');
+    if (!p) {
+      p = document.createElement("pre");
+      p.style = "overflow: scroll; text-align: left;";
+    }
+    document.querySelector("body").appendChild(p);
 
 
-p.innerHTML =
-  "    font-size: " + s.fontSize   + "\n" +
-  "  font-family: " + s.fontFamily + "\n" +
-  "       screen: " + window.innerWidth +"px    x " + window.innerHeight + "px\n" +
-  "       screen: " + (  w).toFixed(3) + "rem w x " + (  h).toFixed(3) + "rem h\n" +
-  "      one rem: " + (1/w).toFixed(3) + "% w   " + (1/h).toFixed(3) + "% h"
-;
-}
+    function getsize(event) {
+      const s = window.getComputedStyle(p);
+      const w = window.innerWidth / parseFloat(s.fontSize);
+      const h = window.innerHeight / parseFloat(s.fontSize);
 
-window.addEventListener("resize", getsize);
-window.addEventListener("load", getsize);
+
+      p.innerHTML =
+        "    font-size: " + s.fontSize + "\n" +
+        "  font-family: " + s.fontFamily + "\n" +
+        "       screen: " + window.innerWidth + "px    x " + window.innerHeight + "px\n" +
+        "       screen: " + (w).toFixed(3) + "rem w x " + (h).toFixed(3) + "rem h\n" +
+        "      one rem: " + (1 / w).toFixed(3) + "% w   " + (1 / h).toFixed(3) + "% h"
+        ;
+    }
+
+    window.addEventListener("resize", getsize);
+  }
+);
